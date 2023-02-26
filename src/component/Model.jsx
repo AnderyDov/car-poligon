d4786ed94a12d0e8de3716a9c9a3db2c3bfb0557
Andrey <dovban.andreyy@rambler.ru>
2023-02-26 20:49:18 +0300

    реализован принцип движения и поворотов

--- src/component/Model.jsx
+++ src/component/Model.jsx
@@ -2,82 +2,99 @@ import { useBox } from '@react-three/cannon';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useRef, useState } from 'react';
-import { Trail } from '@react-three/drei';
 
 export const Model = (props, ttt) => {
     const gltf = useLoader(GLTFLoader, '/img/Car2.glb');
     const [v, setV] = useState({ x: 0, y: 1, z: 30 });
+    const [r, setR] = useState({ x: 0, y: 0, z: 0 });
     const [mesh, api] = useBox(
         () => ({ args: [20, 2, 20], mass: 1, ...props }),
         useRef(null),
     );
 
-    // useFrame(() => {
-    //     mesh.current.position.setPoints(mesh.current);
-    // });
-
     useFrame((state, delta) => {
         if (props.keyPressed.ArrowUp && props.keyPressed.ArrowLeft) {
-            setV({ ...v, z: v.z - 1, x: v.x - 1 });
+            setV({
+                ...v,
+                z: v.z - 2 * Math.cos(r.y),
+                x: v.x + 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y + Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
             return;
         }
         if (props.keyPressed.ArrowUp && props.keyPressed.ArrowRight) {
-            setV({ ...v, z: v.z - 1, x: v.x + 1 });
+            setV({
+                ...v,
+                z: v.z - 2 * Math.cos(r.y),
+                x: v.x + 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y - Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
             return;
         }
         if (props.keyPressed.ArrowDown && props.keyPressed.ArrowLeft) {
-            setV({ ...v, z: v.z + 1, x: v.x - 1 });
+            setV({
+                ...v,
+                z: v.z + 2 * Math.cos(r.y),
+                x: v.x - 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y - Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
             return;
         }
         if (props.keyPressed.ArrowDown && props.keyPressed.ArrowRight) {
-            setV({ ...v, z: v.z + 1, x: v.x + 1 });
+            setV({
+                ...v,
+                z: v.z + 2 * Math.cos(r.y),
+                x: v.x - 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y + Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
+
             return;
         }
 
         if (props.keyPressed.ArrowUp) {
-            setV({ ...v, z: v.z - 1.5 });
+            setV({
+                ...v,
+                z: v.z - 2 * Math.cos(r.y),
+                x: v.x + 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
             return;
         }
         if (props.keyPressed.ArrowLeft) {
-            setV({ ...v, x: v.x - 1.5 });
-            api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y + Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
             return;
         }
         if (props.keyPressed.ArrowDown) {
-            setV({ ...v, z: v.z + 1.5 });
+            setV({
+                ...v,
+                z: v.z + 2 * Math.cos(r.y),
+                x: v.x - 2 * -Math.sin(r.y),
+            });
             api.position.set(v.x, v.y, v.z);
             return;
         }
         if (props.keyPressed.ArrowRight) {
-            setV({ ...v, x: v.x + 1.5 });
-            api.position.set(v.x, v.y, v.z);
+            setR({ ...r, y: r.y - Math.PI / 45 });
+            api.rotation.set(0, r.y, 0);
             return;
         }
     });
 
     return (
-        <Trail
-            width={0.2} // Width of the line
-            color={'hotpink'} // Color of the line
-            length={1} // Length of the line
-            decay={1} // How fast the line fades away
-            local={false} // Wether to use the target's world or local positions
-            stride={0} // Min distance between previous and current point
-            interval={1} // Number of frames to wait before next calculation
-            target={mesh} // Optional target. This object will produce the trail.
-            attenuation={(width) => width} // A function to define the width in each point along it.
-        >
-            <primitive
-                args={[20, 2, 20]}
-                ref={mesh}
-                object={gltf.scene}
-                {...props}
-            />
-        </Trail>
+        <primitive
+            args={[20, 2, 20]}
+            ref={mesh}
+            object={gltf.scene}
+            {...props}
+        />
     );
 };
